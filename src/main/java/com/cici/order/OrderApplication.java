5d8a4113b49b22dfd7c140e20180fca5ae7c2429
Redamancy1 <1792411355@qq.com>
2020-09-20 21:48:30 +0800

    Init: 初始化项目

---
+++ src/main/java/com/cici/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.cici.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
